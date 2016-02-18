8b0d0d4cd15a92fa5cd1fb9fce32b75ddf21a4d9
Seida <Seida@PC.mshome.net>
2016-02-18 10:47:51 -0600

    MI PRIMER Commit

---
+++ HOLA MUNDO/src/HOLA MUNDO.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : HOLA.cpp
+// Author      : DRASLY RAFAEL 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+//METODO SALUDAR
+void saludar (char* nombre){
+
+	cout << "!!! BIENVENIDA A PROGRAMACION !!!" << nombre << endl; // prints !!!BIENVENIDA A PROGRAMACION!!!
+
+}
+
+// METODO PRINCIPAL
+int main (){
+	saludar ("DRASLY");
+
+
+	return 0;
+
+}
